3924581375e04669cdfdfb3ae275187493c34d6c
Katsuyuki Omuro <komuro@fastly.com>
2025-02-06 03:46:17 +0900

    removed unused file

--- single-compute-origin-and-ssr/src/app/AddToCart.js
+++
@@ -1,9 +0,0 @@
-export function AddToCart({action, productId}) {
-  return (
-    <form action={action}>
-        <input type="hidden" name="productId" value={productId} />
-        <button type="submit">Add to Cart</button>
-    </form>
-
-  );
-}
